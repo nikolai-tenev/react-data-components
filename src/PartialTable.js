827900e7ef2f869f14929c8212c4c5036c7b6f42
bh2o <tenev.bh2o@gmail.com>
2017-03-01 14:33:57 +0200

    layout changes, added paging info.

--- src/PartialTable.js
+++ src/PartialTable.js
@@ -12,38 +12,70 @@ export default class PartialTable extends Component {
 
         const {
             page, pageSize, pageNumber,
-            totalPages, sortBy, filterValue,
+            totalPages, totalItems, sortBy, filterValue,
         } = this.props.data;
 
+        const infoStartItem = pageNumber * pageSize;
+        const infoEndItem = infoStartItem + page.length;
+
         return (
-            <div className="container">
+            <div className="table-responsive">
                 <div className="row">
-                    <div className="col-xs-4">
-                        <div>
-                            <label htmlFor="page-menu">Page size:</label>
-                            <select
-                                id="page-menu"
-                                value={pageSize}
-                                onChange={onPageSizeChange}
-                            >
-                                {pageLengthOptions.map(opt =>
-                                    <option key={opt} value={opt}>
-                                        {opt === 0 ? 'All' : opt}
-                                    </option>
-                                )}
-                            </select>
-                        </div>
-                        <div>
-                            <label htmlFor="search-field">Search:</label>
-                            <input
-                                id="search-field"
-                                type="search"
-                                value={filterValue}
-                                onChange={onFilter}
-                            />
+                    <div className="col-sm-6">
+                        <div className="form-group">
+                            <div className="form-inline">
+                                <label className="control-label" htmlFor="search-field">Search:</label>
+                                <input
+                                    id="search-field"
+                                    className="form-control input-sm"
+                                    type="search"
+                                    value={filterValue}
+                                    onChange={onFilter}
+                                />
+                            </div>
                         </div>
                     </div>
-                    <div className="col-xs-8">
+                    <div className="col-sm-6">
+                        <div className="form-group pull-right">
+                            <div className="form-inline">
+                                <label className="control-label" htmlFor="page-menu">Page size:</label>
+                                <div className="input-group">
+                                    <select
+                                        id="page-menu"
+                                        className="form-control input-sm"
+                                        value={pageSize}
+                                        onChange={onPageSizeChange}
+                                    >
+                                        {pageLengthOptions.map(opt =>
+                                            <option key={opt} value={opt}>
+                                                {opt === 0 ? 'All' : opt}
+                                            </option>
+                                        )}
+                                    </select>
+                                    <span className="input-group-addon">items</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <Table
+                            className="table table-bordered table-striped table-hover table-condensed"
+                            dataArray={page}
+                            columns={columns}
+                            keys={keys}
+                            buildRowOptions={buildRowOptions}
+                            sortBy={sortBy}
+                            onSort={onSort}
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-8">
+                        <p>Displaying {infoStartItem}-{infoEndItem} of {totalItems}</p>
+                    </div>
+                    <div className="col-sm-4">
                         <Pagination
                             className="pagination pull-right"
                             currentPage={pageNumber}
@@ -52,15 +84,6 @@ export default class PartialTable extends Component {
                         />
                     </div>
                 </div>
-                <Table
-                    className="table table-bordered"
-                    dataArray={page}
-                    columns={columns}
-                    keys={keys}
-                    buildRowOptions={buildRowOptions}
-                    sortBy={sortBy}
-                    onSort={onSort}
-                />
             </div>
         );
     }
